c8717234e09565b2c01ee358a2f5eeae6307f52e
Mateusz <48615324+ITEFARMAT@users.noreply.github.com>
2021-01-25 11:43:10 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -17,3 +17,26 @@ done
 echo "$MATCH_COUNT match(es) for filter '$PATH_FILTER' found."
 
 
+################################################################
+PATH_FILTER="src/"
+CHANGED_FILES=$(git diff HEAD HEAD~ --name-only)
+MATCH_COUNT=0
+
+echo "Checking for file changes..."
+for FILE in $CHANGED_FILES
+do
+if [[ $FILE == *$PATH_FILTER* ]]; then
+  echo "MATCH:  ${FILE} changed"
+  MATCH_FOUND=true
+  MATCH_COUNT=$(($MATCH_COUNT+1))
+else
+  echo "IGNORE: ${FILE} changed"
+fi
+done
+
+echo "$MATCH_COUNT match(es) for filter '$PATH_FILTER' found."
+if [[ $MATCH_COUNT -gt 0 ]]; then
+echo "##vso[task.setvariable variable=SOURCE_CODE_CHANGED;isOutput=true]true"
+else
+echo "##vso[task.setvariable variable=SOURCE_CODE_CHANGED;isOutput=true]false"
+fi
